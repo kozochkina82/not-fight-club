038d972d2c2789bd8afe6a7967c9947b3e04970c
kozochkina82 <kozochkina82@yandex.ru>
2025-08-25 01:29:04 +0500

    fix: fix links

--- not-fight-club/script.js
+++ not-fight-club/script.js
@@ -1,9 +1,23 @@
+let Name = document.querySelector('.name-field').value;
+
 document.getElementById('character-form').addEventListener('submit', function (event) {
     event.preventDefault();
-    const name = document.querySelector('.name-field').value;
-    localStorage.setItem('characterName', name);
-    window.location.href = '/not-fight-club/start.html';
+    localStorage.setItem('characterName', Name);
+    window.location.href = 'start.html';
 });
 
 
+document.querySelector('.edit-button').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let newName = prompt('Введите новое имя персонажа', '');
+    if (newName === null && newName.trim() !== '') {
+        Name = newName;
+            localStorage.setItem('characterName', newName);
+    }
 
+})
+
+document.querySelector('.submit-button').addEventListener('submit', function (event) {
+    event.preventDefault();
+window.location.href = 'fight.html';
+})
